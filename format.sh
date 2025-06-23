a261bb50bc2d5dfeaaf3f31a03586cdc1ef82e0f
jhyong <jhyong@simitgroup.com>
2025-06-23 15:45:59 +0800

    Update Format Script

--- format.sh
+++ format.sh
@@ -1,2 +1,7 @@
-black simtrain_eco_mini_app_streamlit_sdk/services/resources/*.py
-black simtrain_eco_mini_app_streamlit_sdk/sdk.py
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+source "$SCRIPT_DIR/.venv/bin/activate"
+
+black "$SCRIPT_DIR/simtrain_eco_mini_app_streamlit_sdk/services/resources/"*.py
+black "$SCRIPT_DIR/simtrain_eco_mini_app_streamlit_sdk/sdk.py"
+
+deactivate
